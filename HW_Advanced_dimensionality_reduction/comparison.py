4a19df064c803dedfc11ea39d714fae49e7d5c04
Antonio Oliva <160790965+AntoniooOliva@users.noreply.github.com>
2025-11-02 23:31:08 +0200

    Bike rental dataset regression visualization

--- HW_Advanced_dimensionality_reduction/comparison.py
+++
@@ -1,5 +0,0 @@
-# 1. Comparing linear and non-linear DR (4 points)
-#    Compare PCA and t-SNE methods by visualizing Bike Sharing Rental dataset.
-#    Explore how the different features are shown in the DR components. Build a simple prediction model (for example, MLP or Random Forest)
-#    to predict the count of total rental bikes and compare the performance of the model with the different DR techniques.
-
